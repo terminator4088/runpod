74f566b8cf8cff70070a882f8ede905a32391e58
terminator4088 <43856573+terminator4088@users.noreply.github.com>
2023-09-22 10:45:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -87,7 +87,11 @@ while True:
     print('Relauncher: Launching...')
     if n > 0:
         print(f'\tRelaunch count: {n}')
-    launch_string = "/workspace/stable-diffusion-webui/webui.sh -f --listen --insecure"
+    if [ -z "$A1111" ]; then
+	launch_string = "/workspace/stable-diffusion-webui/webui.sh -f --listen --insecure"
+    else
+	launch_string = "/workspace/stable-diffusion-webui/webui.sh -f --listen"
+    fi
     os.system(launch_string)
     print('Relauncher: Process is ending. Relaunching in 2s...')
     n += 1
