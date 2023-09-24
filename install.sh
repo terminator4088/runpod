696dd635dd124754ed31746e51632a784c6db29f
terminator4088 <43856573+terminator4088@users.noreply.github.com>
2023-09-24 21:39:21 +0200

    l

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 #####	Start Docker Cmd: /bin/bash -c 'if [ ! -f /setup.sh ]; then wget "https://raw.githubusercontent.com/terminator4088/runpod/main/install.sh" -O /setup.sh && chmod +x /setup.sh && /setup.sh; fi'
 
+#
+A1111 = 1
+#
+
 apt update
 apt install -y vim
 
